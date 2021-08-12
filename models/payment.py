54063ee0ec1f4123d088fcb23705296fe3eec772
JoelRamirez2016 <joel.ramirez@correounivalle.edu.co>
2021-08-12 16:32:41 +0000

    fix payment model

--- models/payment.py
+++ models/payment.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class Payment"""
 from base_model import BaseModel, Base
-from order import
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class Payment(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    'type' = Column(String(128), nullable=False)
+    """This class defines a payments by various attributes"""
+    __tablename__ = "payments"
+    type = Column(String(128), nullable=False)
     date = Column(Integer(128), nullable=False)
-    txn_id = Column(Integer(128), ForeignKey("txn.id"))
-    total = Column(Integer(128))
-    order = relationship("Order", backref="order", cascade ="all, delete")
+    txn_id = Column(String(60), nullable=False)
+    total = Column(Integer, nullable=False)
+    order_id = Column(String(60), nullable=False, ForeignKey("orders.id"))
+    order = relationship("Order", backref="payments", cascade ="all, delete")
