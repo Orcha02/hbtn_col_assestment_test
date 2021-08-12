a309ee183189ed4c45b82bc1453483f78f066974
Jaricapa-holberton <2241@holbertonschool.com>
2021-08-12 11:20:55 -0500

    Update: class user v4

--- user.py
+++ user.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    gov_id= Column(int(20), primary_key=True,nullable=False)
+    gov_id= Column(int, primary_key=True,nullable=False)
     name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
     email = Column(String(128), nullable=True)
